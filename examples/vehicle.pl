76e91ce314edd84bbde6abece754ed1b4437f789
Steve Bertrand <steveb@cpan.org>
2022-03-18 10:44:37 -0700

    Added example script that's used for development

---
+++ examples/vehicle.pl
@@ -0,0 +1,16 @@
+use warnings;
+use strict;
+use feature 'say';
+
+use Data::Dumper;
+use Tesla::Vehicle;
+
+my $v = Tesla::Vehicle->new;
+
+#say $v->charge_limit_soc;
+#say $v->charge_limit_soc_min;
+#say $v->charge_limit_soc_std;
+#say $v->charge_limit_soc_max;
+
+print Dumper $v->drive_state;
+1;
