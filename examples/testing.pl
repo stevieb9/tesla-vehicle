b95d71d7537686cfa327352ad6ccca451305f244
Steve Bertrand <steveb@cpan.org>
2022-03-31 09:15:18 -0700

    Tweak to examples/testing.pl

--- examples/testing.pl
+++ examples/testing.pl
@@ -7,21 +7,7 @@ use feature 'say';
 use Data::Dumper;
 use Tesla::Vehicle;
 
-my $x = Tesla::Vehicle->new;
+my $x = Tesla::Vehicle->new(auto_wake => 1);
 
-
-say $x->options;
-
-#say $x->trunk_rear_actuate;
-#say $x->trunk_rear;
-#say "c on: " . $x->climate_on;
-#say "c: " . $x->is_climate_on;
-#
-#say "c off: " . $x->climate_off;
-#say "c: " . $x->is_climate_on;
-#
-#say "du: " . $x->doors_unlock;
-#say "d: " . $x->locked;
-#say "dl: " . $x->doors_lock;
-#say "d: " . $x->locked;
+say $x->gear;
 
