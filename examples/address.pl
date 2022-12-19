7e9555011441aad5a4cec4a73b65c84fc7fc8e46
Steve Bertrand <steveb@cpan.org>
2022-12-18 19:23:01 -0800

    Modified output of address example

--- examples/address.pl
+++ examples/address.pl
@@ -9,5 +9,19 @@ use Tesla::Vehicle;
 
 my $car = Tesla::Vehicle->new(auto_wake => 1);
 
-print Dumper  $car->address;
+my $address = $car->address;
 
+print "\n";
+
+say "road: $address->{road}";
+
+for (sort keys %$address) {
+    next if $_ eq 'road';
+    next if /ISO3166/;
+    say "$_: $address->{$_}";
+}
+
+my $gear = $car->gear;
+say "gear: $gear";
+
+print "\n";
